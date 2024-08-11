ea8fa59a8bfcec6183ada599fc68491e99fb373c
Rahul Kashyap <77141265+RahulDeswalOne@users.noreply.github.com>
2024-08-11 14:09:16 +0530

    Update install-k8s.sh with version

--- install-k8s.sh
+++ install-k8s.sh
@@ -41,7 +41,7 @@ curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update -y
-sudo apt-get install -y kubelet="1.29.0-*" kubectl="1.29.0-*" kubeadm="1.29.0-*"
+sudo apt-get install -y kubelet="1.29.7-*" kubectl="1.29.7-*" kubeadm="1.29.7-*"
 sudo apt-get update -y
 sudo apt-get install -y jq
 
